6eb112211a537cee62fb4972799c52fffb6138b4
Nahid Akbar <civilian0746@gmail.com>
2016-05-11 11:44:32 +1000

    Update style.js

--- style.js
+++ style.js
@@ -8,15 +8,20 @@ $(document).ready(function(){
                     '<img src="//ausftas.github.io/example-api-common/images/index.png" alt="australian government, australia: open for business"><div class="brand">Free Trade Agreement Portal</div></div></div>' + 
                     '<div class="header-divider-strap"></div>');
 
-  var example = 'https://github.com/AusFTAs/' + window.location.toString().match(/[/](example-api-.*)[/]/)[1];
+  var example = null;
 
-  $('#header').append('<div class="ribbon left red"><a href="' + example + '">Clone me on GitHub</a></div>');
-  $('#header').append('<div class="ribbon right red"><a href="' + example + '">Clone me on GitHub</a></div>');
+  if (window.location.toString().match(/[/](example-api-.*)[/]/)[1])
+  {
+    var example = 'https://github.com/AusFTAs/' + window.location.toString().match(/[/](example-api-.*)[/]/)[1];
+  }
+
+  example && $('#header').append('<div class="ribbon left red"><a href="' + example + '">Clone me on GitHub</a></div>');
+  example && $('#header').append('<div class="ribbon right red"><a href="' + example + '">Clone me on GitHub</a></div>');
 
   $('#footer').html('<br /><div class="footer-strap">' +
   '<div class="footer-content">' +
     '<ul class="footer-links">' +
-      '<li><a href="' + example + '">Clone example on Github</a> | </li>' +
+      (example? '<li><a href="' + example + '">Clone example on Github</a> | </li>' : '') +
       '<li><a href="http://ausftas.github.io/api/">API</a> | </li>' +
       '<li><a href="https://ftaportal.dfat.gov.au">DFAT FTA Portal</a> | </li>' +
       '<li><a href="http://dfat.gov.au/trade/agreements/Pages/trade-agreements.aspx">DFAT Free Trade Agreements</a> | </li>' +
